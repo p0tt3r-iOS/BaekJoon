fe52a283126920550b01b3f4ee14b139ae8c438d
AlphaAOmega <france998811@gmail.com>
2021-06-16 00:07:49 +0900

    Solve Programmers Level 2 algorithm

--- Programmers/210615/P42842.swift
+++ Programmers/210615/P42842.swift
@@ -11,10 +11,14 @@ func soultionP42842(_ brown: Int, _ yellow: Int) -> [Int] {
     let sum = brown + yellow
     var divisor: [Int] = []
     for i in 1..<sum {
-        if sum % i == 0 {
-            divisor.append(i)
+        divisor.append(i)
+    }
+    for j in divisor {
+        if (j - 2) * ((sum / j) - 2) == yellow {
+            return [sum / j, j]
         }
     }
     
-    return [divisor.max()!, sum / divisor.max()!]
+    return []
 }
+
